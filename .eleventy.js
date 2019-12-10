dbfce13a6f28066bd25c40cc2f98746c54355ec2
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2019-12-10 21:25:43 +0100

    chore: minor updates

--- .eleventy.js
+++ .eleventy.js
@@ -3,20 +3,23 @@ const fs = require('fs');
 const globCss = require('glob-fs')({ gitignore: false });
 const globJs = require('glob-fs')({ gitignore: false });
 
+const cssPath = "source/static/css";
+const jsPath = "source/static/js";
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("cssIncludes", function () {
     let cssIncludes = "";
 
-    globCss.readdirSync('source/static/css/*.css').forEach(function (cssFile) {
+    globCss.readdirSync(`${cssPath}/*.css`).forEach(function (cssFile) {
       if (cssFile.indexOf('inline') > 0) {
         let fileContent = fs.readFileSync(cssFile, 'UTF-8');
         cssIncludes +=
           `<style type="text/css">/*<![CDATA[*/<!-- ${fileContent} -->/*]]>*/</style>`;
       } else {
-        cssFile = cssFile.replace('source/static/css/', '');
+        cssFile = cssFile.replace(cssPath, "/css");
         cssIncludes +=
-          `<link rel="stylesheet" href="/css/${cssFile}" media="print" onload="this.media='all'">` +
-          `<noscript><link rel="stylesheet" href="/css/${cssFile}"></noscript>`;
+          `<link rel="stylesheet" href="${cssFile}" media="print" onload="this.media='all'">` +
+          `<noscript><link rel="stylesheet" href="${cssFile}"></noscript>`;
       }
     });
 
@@ -26,9 +29,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("jsIncludes", function () {
     let jsIncludes = "";
 
-    globJs.readdirSync('source/static/js/*.js').forEach(function (jsFile) {
-      jsFile = jsFile.replace('source/static/js/', '');
-      jsIncludes += `<script src="/js/${jsFile}" defer></script>`;
+    globJs.readdirSync(`${jsPath}/*.js`).forEach(function (jsFile) {
+      jsFile = jsFile.replace(jsPath, "/js");
+      jsIncludes += `<script src="${jsFile}" defer></script>`;
     });
 
     return jsIncludes;
