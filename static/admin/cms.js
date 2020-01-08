fbe9834929033f266a2f0f0aafd927ffaf7fca69
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2020-01-08 22:13:52 +0100

    chore: remove console.log

--- static/admin/cms.js
+++ static/admin/cms.js
@@ -1,6 +1,4 @@
 import CMS from 'netlify-cms';
 import idWidget from 'netlify-cms-widget-simple-uuid';
 
-console.log('idWidget', idWidget);
-
 CMS.registerWidget('id', idWidget.IdControl, idWidget.IdPreview);
